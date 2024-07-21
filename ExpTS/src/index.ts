02cdcdc94bc07a375243fae08a64010223bdca7c
lucasmesmo <darciolucas701@gmail.com>
2024-07-20 21:47:26 -0400

    Descrição das alterações

--- ExpTS/src/index.ts
+++ ExpTS/src/index.ts
@@ -9,7 +9,7 @@ iniciando configs de ts
 
 */
 
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -17,8 +17,23 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+/*
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello world!:D");
+});
+
+app.listen(PORT, () => {
+	console.log(`Express app iniciada na porta ${PORT}.`);
+});
+*/
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+	console.log(`Requisição ${req.method} ${req.url}`);
+	next();
+});
+
+app.get("/", (req, res) => {
+	res.send("Hello world!");
 });
 
 app.listen(PORT, () => {
